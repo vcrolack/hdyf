3e0a94a974f4588c10c4dfd2111dc255719982c9
Victor Rolack <victorolack@gmail.com>
2024-10-19 17:50:23 -0300

    feat: model resume detail

--- src/features/Home/components/detail-count/DetailRecently.tsx
+++ src/features/Home/components/detail-count/DetailRecently.tsx
@@ -6,12 +6,17 @@ export const DetailRecently = () => {
     >
       <div
         id="resume-count-detail-recently-mean"
-        className="w-1/2 h-full bg-color-primary-hover rounded-lg"
-      ></div>
+        className="w-1/2 h-full p-4 bg-color-primary-hover rounded-lg flex flex-col gap-y-8"
+      >
+        <p className="font-bold text-3xl">Average Mood</p>
+        <p className="font-bold text-4xl text-center">Neutral</p>
+      </div>
       <div
         id="resume-count-detail-recently-detail"
-        className="w-1/2 h-full bg-color-primary-hover rounded-lg"
-      ></div>
+        className="w-1/2 h-full bg-color-primary-hover rounded-lg flex justify-center items-center"
+      >
+        <p className="text-7xl">😐</p>
+      </div>
     </div>
   );
 };
